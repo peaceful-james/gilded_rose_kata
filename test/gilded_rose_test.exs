59c8acfcef92bb68fe81fb0b164fe4650a56bb12
James Gough <james@peacefulprogramming.dev>
2025-04-01 13:55:04 +0100

    writed sanity/safety test

--- test/gilded_rose_test.exs
+++ test/gilded_rose_test.exs
@@ -1,10 +1,30 @@
 defmodule GildedRoseTest do
   use ExUnit.Case
+  alias GildedRose.TextTestFixture
 
   test "begin the journey of refactoring" do
     items = [%Item{name: "foo", sell_in: 0, quality: 0}]
     GildedRose.update_quality(items)
     %{name: firstItemName} = List.first(items)
     assert "fixme" == firstItemName
+  end
+
+  describe "bulk black-box test" do
+    test "has not broken" do
+      items = TextTestFixture.assorted_items()
+      result = GildedRose.update_quality(items)
+
+      assert result == [
+               %Item{name: "+5 Dexterity Vest", sell_in: 9, quality: 19},
+               %Item{name: "Aged Brie", sell_in: 1, quality: 1},
+               %Item{name: "Elixir of the Mongoose", sell_in: 4, quality: 6},
+               %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 80},
+               %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: -1, quality: 80},
+               %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 14, quality: 21},
+               %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 50},
+               %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 4, quality: 50},
+               %Item{name: "Conjured Mana Cake", sell_in: 2, quality: 5}
+             ]
+    end
   end
 end
--- test/support/texttest_fixture.ex
+++ test/support/texttest_fixture.ex
@@ -1,8 +1,6 @@
 defmodule GildedRose.TextTestFixture do
-  def run() do
-    IO.puts("OMGHAI!")
-
-    items = [
+  def assorted_items do
+    [
       %Item{name: "+5 Dexterity Vest", sell_in: 10, quality: 20},
       %Item{name: "Aged Brie", sell_in: 2, quality: 0},
       %Item{name: "Elixir of the Mongoose", sell_in: 5, quality: 7},
@@ -14,6 +12,12 @@ defmodule GildedRose.TextTestFixture do
       # This Conjured item does not work properly yet
       %Item{name: "Conjured Mana Cake", sell_in: 3, quality: 6}
     ]
+  end
+
+  def run() do
+    IO.puts("OMGHAI!")
+
+    items = assorted_items()
 
     %{report_lines: report_lines} =
       Enum.reduce(0..1, %{items: items, report_lines: []}, fn day,
